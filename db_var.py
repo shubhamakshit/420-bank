231a5f36679e6559a3a4c4af6c4c3b2459c2c277
Akshit shubham <121078566+shubhamakshit@users.noreply.github.com>
2024-10-25 16:40:15 +0100

    Update db_var.py

--- db_var.py
+++ db_var.py
@@ -1,26 +1,46 @@
-# db_var.py
 import hashlib
 import os
 import MySQLdb
+import psycopg2  # Import for PostgreSQL support
 from dbutils.pooled_db import PooledDB
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
 
-db_pool = PooledDB(
-    creator=MySQLdb,
-    maxconnections=100,
-    mincached=10,
-    maxcached=20,
-    blocking=True,
-    host=os.getenv('DB_HOST'),
-    user=os.getenv('DB_USER'),
-    passwd=os.getenv('DB_PASSWORD'),
-    db=os.getenv('DB_NAME'),
-    maxshared=0,
-    ping=0,
-)
+# Get the database type from environment variables (default to MySQL if not specified)
+db_type = os.getenv('DB_TYPE', 'mysql').lower()
+
+# Configure the database connection pool based on the selected database type
+if db_type == 'mysql':
+    db_pool = PooledDB(
+        creator=MySQLdb,
+        maxconnections=100,
+        mincached=10,
+        maxcached=20,
+        blocking=True,
+        host=os.getenv('DB_HOST'),
+        user=os.getenv('DB_USER'),
+        passwd=os.getenv('DB_PASSWORD'),
+        db=os.getenv('DB_NAME'),
+        maxshared=0,
+        ping=0,
+    )
+elif db_type == 'postgresql':
+    db_pool = PooledDB(
+        creator=psycopg2,
+        maxconnections=100,
+        mincached=10,
+        maxcached=20,
+        blocking=True,
+        host=os.getenv('DB_HOST'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_NAME'),
+        maxshared=0,
+    )
+else:
+    raise ValueError("Unsupported DB_TYPE specified. Use 'mysql' or 'postgresql'.")
 
 class SpecialUser:
     def __init__(self):
